bf8e615f9b56ee971f77df483f559f11fadca8f8
CODE-G-ROOT <juandavidavilaravelo856@gmail.com>
2023-12-02 11:08:53 -0500

    feat: :seedling: add components files
    these files has been named depending of on their funcionality, in this case, for the routers

---
+++ src/pages/SingIn/index.jsx
@@ -0,0 +1,12 @@
+function SingIn() {
+
+    return (
+        <>
+            <div className='bg-red-100'>
+                SingIn
+            </div>
+        </>
+    )
+};
+
+export default SingIn;
