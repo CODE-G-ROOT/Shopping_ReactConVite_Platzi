90e421b9313a4fbe32cc10d79feb36c9b472d034
CODE-G-ROOT <juandavidavilaravelo856@gmail.com>
2023-12-02 11:27:03 -0500

    feat: :sparkles: importation of components
    The components has been imported and will be used in the future

--- src/pages/app/Index.jsx
+++ src/pages/app/Index.jsx
@@ -1,11 +1,24 @@
+import Home from '../home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SingIn from '../SingIn'
+
 import './App.css'
+
 
 function App() {
 
   return (
     <>
       <div className='bg-red-100'>
-        Hola Mundo bebé
+        <MyAccount></MyAccount>
+        <Home></Home>
+        <MyOrder></MyOrder>
+        <MyOrders></MyOrders>
+        <NotFound></NotFound>
+        <SingIn></SingIn>
       </div>
     </>
   )
